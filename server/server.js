ebb2109216a92780cfee3f2395d1981e0406435b
Andy Kim <andyhyukim@gmail.com>
2025-03-11 15:14:53 -0400

    Add relationship endpoints

--- server/server.js
+++ server/server.js
@@ -93,7 +93,149 @@ app.get('/api/planets/:id', async (req, res) => {
     }
 });
 
+app.get('/api/films/:id/characters', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const client = await MongoClient.connect(url)
+        const db = client.db(dbName)
+        const collection = db.collection("films_characters")
+        const films_characters = await collection.aggregate([
+            {
+                $lookup: {
+                    from: "characters",
+                    localField: "character_id",
+                    foreignField: "id",
+                    as: "character_details"
+                }
+            },
+            {
+                $match: {
+                    film_id: +id
+                }
+            }
+        ]).toArray();
+        res.json(films_characters);
+    } catch(err) {
+        console.error("Error:", err)
+        res.status(500).send("couldn't get items");
+    }
+});
+
+app.get('/api/films/:id/planets', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const client = await MongoClient.connect(url)
+        const db = client.db(dbName)
+        const collection = db.collection("films_planets")
+        const films_planets = await collection.aggregate([
+            {
+                $lookup: {
+                    from: "planets",
+                    localField: "planet_id",
+                    foreignField: "id",
+                    as: "planet_details"
+                }
+            },
+            {
+                $match: {
+                    film_id: +id
+                }
+            }
+        ]).toArray();
+        res.json(films_planets);
+    } catch(err) {
+        console.error("Error:", err)
+        res.status(500).send("couldn't get items");
+    }
+});
+
+app.get('/api/characters/:id/films', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const client = await MongoClient.connect(url)
+        const db = client.db(dbName)
+        const collection = db.collection("films_characters")
+        const films_characters = await collection.aggregate([
+            {
+                $lookup: {
+                    from: "films",
+                    localField: "film_id",
+                    foreignField: "id",
+                    as: "film_details"
+                }
+            },
+            {
+                $match: {
+                    character_id: +id
+                }
+            }
+        ]).toArray();
+        res.json(films_characters);
+    } catch(err) {
+        console.error("Error:", err)
+        res.status(500).send("couldn't get items");
+    }
+});
+
+app.get('/api/planets/:id/films', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const client = await MongoClient.connect(url)
+        const db = client.db(dbName)
+        const collection = db.collection("films_planets")
+        const films_planets = await collection.aggregate([
+            {
+                $lookup: {
+                    from: "films",
+                    localField: "film_id",
+                    foreignField: "id",
+                    as: "film_details"
+                }
+            },
+            {
+                $match: {
+                    planet_id: +id
+                }
+            }
+        ]).toArray();
+        res.json(films_planets);
+    } catch(err) {
+        console.error("Error:", err)
+        res.status(500).send("couldn't get items");
+    }
+});
+
+// Return the characters born on a given planet
+app.get('/api/planets/:id/characters', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const client = await MongoClient.connect(url)
+        const db = client.db(dbName)
+        const collection = db.collection("characters")
+        const planet_characters = await collection.aggregate([
+            {
+                $lookup: {
+                    from: "planets",
+                    localField: "homeworld",
+                    foreignField: "id",
+                    as: "planet_details"
+                }
+            },
+            {
+                $match: {
+                    homeworld: +id
+                }
+            }
+        ]).toArray();
+        res.json(planet_characters);
+    } catch(err) {
+        console.error("Error:", err)
+        res.status(500).send("couldn't get items");
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+// db.characters.aggregate([{$lookup: {from: "films_characters", localField: "id", foreignField: "character_id", as: "films_characters_alias"}}])
