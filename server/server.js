df78fdde3c188f32c9805c19b88ec7eb71caa96e
Andy Kim <andyhyukim@gmail.com>
2025-03-11 13:50:55 -0400

    Add needed libraries for server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const port = process.env.EXPRESS_PORT;
 
 const app = express();
-const PORT = 3000;
+app.use(cors());
+app.use(express.json());
 
 app.get('/api/planets', (req, res) => {
     res.json({ message: 'hi' });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
