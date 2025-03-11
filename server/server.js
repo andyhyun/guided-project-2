6e9f6d22f97d6c124b0df848013798ac0381a27a
Bryan Lecza <blecza@travelers.com>
2025-03-11 14:03:59 -0400

    made get request for films, planets, and characters

--- server/server.js
+++ server/server.js
@@ -12,9 +12,45 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/planets', (req, res) => {
-    res.json({ message: 'hi' });
+app.get('/api/characters', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url)
+        const db = client.db(dbName)
+        const collection = db.collection("characters")
+        const characters = await collection.find({}).toArray();
+        res.json(characters);
+    } catch(err) {
+        console.error("Error:", err)
+        res.status(500).send("couldn't get items");
+    }
 });
+
+app.get('/api/films', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url)
+        const db = client.db(dbName)
+        const collection = db.collection("films")
+        const films = await collection.find({}).toArray();
+        res.json(films);
+    } catch(err) {
+        console.error("Error:", err)
+        res.status(500).send("couldn't get items");
+    }
+});
+
+app.get('/api/planets', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url)
+        const db = client.db(dbName)
+        const collection = db.collection("planets")
+        const planets = await collection.find({}).toArray();
+        res.json(planets);
+    } catch(err) {
+        console.error("Error:", err)
+        res.status(500).send("couldn't get items");
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
