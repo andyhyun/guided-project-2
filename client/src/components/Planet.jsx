e8ce2f644241a9dcd096d7d47d6edc6b7a8a7b2c
Bryan Lecza <blecza@travelers.com>
2025-03-12 09:35:54 -0400

    finished planet component

--- client/src/components/Planet.jsx
+++ client/src/components/Planet.jsx
@@ -4,35 +4,54 @@ import { useParams } from 'react-router-dom';
 export const Planet = () => {
     
     const [planet, setPlanet] = useState({});
-    const [characters, setCharacters] = useState({});
+    const [characters, setCharacters] = useState([]);
     const [films, setFilms] = useState([]);
     let params = useParams();
     
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchPlanet = async () => {
             const planet_data = await fetch(`${import.meta.env.VITE_SWAPI_URL}/planets/${params.id}`);
             const json_planet_data = await planet_data.json();
-            const films_data = await fetch(`${import.meta.env.VITE_SWAPI_URL}/planets/${params.id}/films`);
-            const json_films_data = await films_data.json();
-            const characters_data = await fetch(`${import.meta.env.VITE_SWAPI_URL}/planets/${params.id}/characters`);
-            const json_characters_data = await characters_data.json();
             
             setPlanet(json_planet_data);
-            setFilms(json_films_data);
-            setCharacters(json_characters_data);
         };
-        fetchData();
+        fetchPlanet();
+    }, []);
+
+    useEffect(() => {
+        const fetchCharacters = async () => {
+            const characters_data = await fetch(`${import.meta.env.VITE_SWAPI_URL}/planets/${params.id}/characters`);
+            const json_characters_data = await characters_data.json();
+            setCharacters(json_characters_data);
+        }
+        fetchCharacters();
+    }, []);
+
+    useEffect(() => {
+        const fetchFilms = async () => {
+            const films_data = await fetch(`${import.meta.env.VITE_SWAPI_URL}/planets/${params.id}/films`);
+            const json_films_data = await films_data.json();
+            setFilms(json_films_data);
+        }
+        fetchFilms();
     }, []);
 
     return (
         <>
             <h1>{planet.name}</h1>
+            <p>{`Climate: ${planet.climate}`}</p>
+            <p>{`Diameter: ${planet.diameter}`}</p>
+            <p>{`Gravity: ${planet.gravity}`}</p>
+            <p>{`Terrain: ${planet.terrain}`}</p>
             <h2>Films</h2>
             <div>
+                {console.log(characters)}
                 {films.map((film) => (<div key={film.film_id}>{film.film_details[0].title}</div>))}
             </div>
             <h2>Characters</h2>
-            <div>{planet.name}</div>
+            <div>
+                {characters.map((character) => (<div key={character.id}>{character.name}</div>))}
+            </div>
         </>
     );
 }
