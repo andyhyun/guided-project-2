fb443cd3e71467416302123c836e03defbb69ac5
Andy Kim <andyhyukim@gmail.com>
2025-03-12 09:13:28 -0400

    Add general info to character page

--- client/src/components/Character.jsx
+++ client/src/components/Character.jsx
@@ -3,35 +3,55 @@ import { useParams } from 'react-router';
 
 export const Character = () => {
     const [character, setCharacter] = useState({});
-    const [planet, setPlanet] = useState('');
+    const [planet, setPlanet] = useState({});
     const [films, setFilms] = useState([]);
     let params = useParams();
     
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchCharacter = async () => {
             const character_data = await fetch(`${import.meta.env.VITE_SWAPI_URL}/characters/${params.id}`);
             const json_character_data = await character_data.json();
+            setCharacter(json_character_data);
+        };
+        fetchCharacter();
+    }, []);
+    
+    useEffect(() => {
+        const fetchFilms = async () => {
             const films_data = await fetch(`${import.meta.env.VITE_SWAPI_URL}/characters/${params.id}/films`);
             const json_films_data = await films_data.json();
-            const planet_data = await fetch(`${import.meta.env.VITE_SWAPI_URL}/planets/${json_character_data.homeworld}`);
-            const json_planet_data = await planet_data.json();
-            setCharacter(json_character_data);
-            setPlanet(json_planet_data);
             setFilms(json_films_data);
-            console.log(json_planet_data);
         };
-        fetchData();
+        fetchFilms();
     }, []);
+    
+    useEffect(() => {
+        const fetchPlanet = async () => {
+            const planet_data = await fetch(`${import.meta.env.VITE_SWAPI_URL}/planets/${character?.homeworld}`);
+            const json_planet_data = await planet_data.json();
+            setPlanet(json_planet_data);
+        };
+        fetchPlanet();
+    }, [character]);
 
     return (
         <>
-            <h1>{character.name}</h1>
-            <h2>Films</h2>
-            <div>
-                {films.map((film) => (<div key={film.film_id}>{film.film_details[0].title}</div>))}
-            </div>
-            <h2>Homeworld</h2>
-            <div>{planet.name}</div>
+            <h1 id="name">{character?.name}</h1>
+            <section id="generalInfo">
+                <p>Height: <span id="height">{character?.height} cm</span></p>
+                <p>Mass: <span id="mass">{character?.mass} kg</span></p>
+                <p>Born: <span id="height">{character?.birth_year}</span></p>
+            </section>
+            <section id="planets">
+                <h2>Homeworld</h2>
+                <p><span id="homeworld">{planet?.name}</span></p>
+            </section>
+            <section id="films">
+                <h2>Films appeared in</h2>
+                <ul>
+                    {films.map((film) => (<li key={film.film_id}>{film.film_details[0].title}</li>))}
+                </ul>
+            </section>
         </>
     );
 };
